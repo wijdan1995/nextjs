56267686ffe526fb89529ec058b1963ededee212
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-25 01:17:51 +0300

    Clean URLs with Dynamic Routing

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,33 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
 
-// Dynamic Pages
+// // Dynamic Pages
+// const PostLink = props => (
+//     <li>
+//         <Link href={`/post?title=${props.title}`}>
+//             <a>{props.title}</a>
+//         </Link>
+//     </li>
+// );
+
+// Clean URL using [id]
 const PostLink = props => (
     <li>
-        <Link href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
+        <Link href="/p/[id]" as={`/p/${props.id}`}>
+            <a>{props.id}</a>
         </Link>
     </li>
 );
+
+// props changed from title to id
 export default function Blog() {
     return (
         <Layout>
             <h1>My Blog</h1>
             <ul>
-                <PostLink title="Hello Next.js" />
-                <PostLink title="Learn Next.js is awesome" />
-                <PostLink title="Deploy apps with Zeit" />
+                <PostLink id="Hello Next.js" />
+                <PostLink id="Learn Next.js is awesome" />
+                <PostLink id="Deploy apps with Zeit" />
             </ul>
         </Layout>
     );
