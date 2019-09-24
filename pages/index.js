85262787b54fcab06446cca0ee20ae8dc7f66c47
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-25 01:31:53 +0300

    Fetching Data for Pages & install isomorphic-unfetch

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
 // // Dynamic Pages
 // const PostLink = props => (
@@ -10,25 +11,56 @@ import Link from 'next/link';
 //     </li>
 // );
 
-// Clean URL using [id]
-const PostLink = props => (
-    <li>
-        <Link href="/p/[id]" as={`/p/${props.id}`}>
-            <a>{props.id}</a>
-        </Link>
-    </li>
+// // Clean URL using [id]
+// const PostLink = props => (
+//     <li>
+//         <Link href="/p/[id]" as={`/p/${props.id}`}>
+//             <a>{props.id}</a>
+//         </Link>
+//     </li>
+// );
+
+// // props changed from title to id
+// export default function Blog() {
+//     return (
+//         <Layout>
+//             <h1>My Blog</h1>
+//             <ul>
+//                 <PostLink id="Hello Next.js" />
+//                 <PostLink id="Learn Next.js is awesome" />
+//                 <PostLink id="Deploy apps with Zeit" />
+//             </ul>
+//         </Layout>
+//     );
+// }
+
+// fetch data from API
+// using the shows props to get the data
+const Index = props => (
+    <Layout>
+        <h1>Batman TV Shows</h1>
+        <ul>
+            {props.shows.map(show => (
+                <li key={show.id}>
+                    <Link href="/p/[id]" as={`/p/${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </Layout>
 );
 
-// props changed from title to id
-export default function Blog() {
-    return (
-        <Layout>
-            <h1>My Blog</h1>
-            <ul>
-                <PostLink id="Hello Next.js" />
-                <PostLink id="Learn Next.js is awesome" />
-                <PostLink id="Deploy apps with Zeit" />
-            </ul>
-        </Layout>
-    );
-}
+// get thr data as props shows
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+
+    console.log(`Show data fetched. Count: ${data.length}`);
+
+    return {
+        shows: data.map(entry => entry.show)
+    };
+};
+
+export default Index;
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,40 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
+// import Layout from '../../components/MyLayout';
+
+// export default function Post() {
+//     const router = useRouter();
+
+//     return (
+//         <Layout>
+//             <h1>{router.query.id}</h1>
+//             <p>This is the blog post content.</p>
+//         </Layout>
+//     );
+// }
+
+// fetch data content view
 import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
 
-export default function Post() {
-    const router = useRouter();
+const Post = props => (
+    <Layout>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        <img src={props.show.image.medium} />
+    </Layout>
+);
 
-    return (
-        <Layout>
-            <h1>{router.query.id}</h1>
-            <p>This is the blog post content.</p>
-        </Layout>
-    );
-}
+// save the res in show props
+Post.getInitialProps = async function (context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log(`Fetched show: ${show.name}`);
+
+    return { show };
+};
+
+export default Post;
+
+
